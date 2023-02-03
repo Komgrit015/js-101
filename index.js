689a1d4453df5cbf5a4245505aef9d71c85c117b
Komgrit015 <ford_za007@hotmail.com>
2023-02-03 11:12:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Variables 
+
+const myName = 'Komgrit' ;
+let height = 175 ;
+let isMale = true ;
+let city = null ;
+let bigNumber = 10n ;
+
+console.log(myName, height, isMale, city, bigNumber);
