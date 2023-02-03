4861bb4a5f1785544922df7a0e0c12c0cc1ec884
Komgrit015 <ford_za007@hotmail.com>
2023-02-03 11:42:52 +0700

    Object and Array

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # js-101
 JavaScript for Beginner 
 
+<<<<<<< HEAD
 การประกาศตัวแปร Variables //first commit
+=======
+>>>>>>> 62fc15e (Object and Array)
 
 
 
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 // Variables 
 
-const myName = 'Komgrit' ;
-let height = 175 ;
-let isMale = true ;
-let city = null ;
-let bigNumber = 10n ;
+// const myName = 'Komgrit' ;
+// let height = 175 ;
+// let isMale = true ;
+// let city = null ;
+// let bigNumber = 10n ;
 
-console.log(myName, height, isMale, city, bigNumber);
+// console.log(myName, height, isMale, city, bigNumber);
+
+
+
+// Object 
+
+const person = {
+    name: 'Komgrit',
+    height: 175,
+    city: 'Bangkok'
+};
+
+console.log(person); //การเข้าถึง object
+console.log(person.name); //การเข้าถึง property ใน object
+
+person.height = 180; //การเปลี่ยนค่า property
+console.log(person);
+
+person.weight = 70; //การเพิ่มค่าเข้าไปใน object
+console.log(person);
+
+
+// Array 
+
+const number = [5, 10, 15, 20]; // การสร้าง Array
+console.log(number);
+console.log(number[2]); //การเข้าถึง index ใน array
+
+
+
+
